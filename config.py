4714bcb00ab15bcf6df914bb1a7f2d4f577246fe
unknown <yoon07817@gmail.com>
2026-06-09 18:46:18 +0630

    Week 3 RSS feed collection completed

---
+++ config.py
@@ -0,0 +1,27 @@
+RSS_FEEDS = [
+    {
+        "category": "Security",
+        "source": "The Hacker News",
+        "url": "https://feeds.feedburner.com/TheHackersNews"
+    },
+    {
+        "category": "Security",
+        "source": "Krebs on Security",
+        "url": "https://krebsonsecurity.com/feed/"
+    },
+    {
+        "category": "Security",
+        "source": "SANS Internet Storm Center",
+        "url": "https://isc.sans.edu/rssfeed.xml"
+    },
+    {
+        "category": "Security",
+        "source": "BleepingComputer",
+        "url": "https://www.bleepingcomputer.com/feed/"
+    },
+    {
+        "category": "Privacy",
+        "source": "EFF Deeplinks",
+        "url": "https://www.eff.org/rss/updates.xml"
+    }
+]
